c783a88b1cff389c29fde2368c8ffc7dd0d6c22d
saltire sable <saltiresable@gmail.com>
2021-02-07 18:54:38 -0500

    Add second oscillator, filters, and waveforms

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+public enum WaveType {
+  Sine,
+  Square,
+  Sawtooth,
+  Noise,
+};
+
 public class Oscillator : MonoBehaviour {
   const double TAU = Mathf.PI * 2.0;
 
@@ -11,20 +18,30 @@ public class Oscillator : MonoBehaviour {
   double phase;
   double sampleFrequency = 48000;
 
+  public WaveType wave = WaveType.Sine;
+
   public float gain;
   public float volume = 0.1f;
 
   public float[] frequencies = { 440, 494, 554, 587, 659, 740, 831, 880 };
   int freqIndex = 0;
 
+  System.Random rand;
+
+  void Awake() {
+    rand = new System.Random();
+  }
+
   void Update() {
     if (Input.GetKeyDown(KeyCode.Space)) {
-      gain = volume;
-      frequency = frequencies[freqIndex];
-      freqIndex = (freqIndex + 1) % frequencies.Length;
-    }
-    if (Input.GetKeyUp(KeyCode.Space)) {
-      gain = 0;
+      if (gain == 0) {
+        gain = volume;
+        frequency = frequencies[freqIndex];
+        freqIndex = (freqIndex + 1) % frequencies.Length;
+      }
+      else {
+        gain = 0;
+      }
     }
   }
 
@@ -34,7 +51,21 @@ public class Oscillator : MonoBehaviour {
     for (int i = 0; i < data.Length; i += channels) {
       phase = (phase + increment) % TAU;
 
-      data[i] = (float)(gain * Mathf.Sin((float)phase));
+      float value = 0;
+      if (wave == WaveType.Sine) {
+        value = Mathf.Sin((float)phase);
+      }
+      else if (wave == WaveType.Square) {
+        value = Mathf.Sign(Mathf.Sin((float)phase));
+      }
+      else if (wave == WaveType.Sawtooth) {
+        value = (float)(phase / TAU) * 2 - 1;
+      }
+      else if (wave == WaveType.Noise) {
+        value = (float)rand.NextDouble() * 2 - 1;
+      }
+
+      data[i] += (float)(value * gain);
 
       if (channels == 2) {
         data[i + 1] = data[i];
