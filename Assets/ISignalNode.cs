05133783f422815fd476a69f984371a731decf49
saltire sable <saltiresable@gmail.com>
2021-02-20 12:16:39 -0500

    Add input/output prefabs

--- Assets/ISignalNode.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections.Generic;
-
-public interface ISignalNode {
-  double GetValue(double sample, Stack<ISignalNode> nodes);
-  double[] GetValues(double sample, int count, Stack<ISignalNode> nodes);
-}
