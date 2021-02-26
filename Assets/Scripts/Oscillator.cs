df62c05e47bee6a442d9114d131d5350a61cba61
saltire sable <saltiresable@gmail.com>
2021-02-25 22:59:10 -0500

    Add volume and waveform knobs to oscillator

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,23 +6,23 @@ using UnityEngine;
 
 public enum WaveType {
   Sine,
-  Square,
-  Sawtooth,
+  Pulse,
+  Sloped,
   Noise,
 };
 
 public class Oscillator : SignalNode {
-  public SignalInput frequencyAdjustInput;
-  public SignalInput volumeAdjustInput;
+  public SignalInput frequencyInput;
+  public SignalInput volumeInput;
+  public SignalInput waveformInput;
   public RangeControl frequencyKnob;
+  public RangeControl volumeKnob;
+  public RangeControl waveformKnob;
   public Button sineButton;
-  public Button squareButton;
-  public Button sawtoothButton;
+  public Button pulseButton;
+  public Button slopedButton;
   public Button noiseButton;
   Button[] buttons;
-
-  public float volume = 1;
-  // public float volumeAdjustSensitivity = .1f;
 
   public WaveType wave = WaveType.Sine;
 
@@ -39,11 +39,11 @@ public class Oscillator : SignalNode {
     if (wave == WaveType.Sine) {
       sineButton.SetGlow(true);
     }
-    else if (wave == WaveType.Square) {
-      squareButton.SetGlow(true);
+    else if (wave == WaveType.Pulse) {
+      pulseButton.SetGlow(true);
     }
-    else if (wave == WaveType.Sawtooth) {
-      sawtoothButton.SetGlow(true);
+    else if (wave == WaveType.Sloped) {
+      slopedButton.SetGlow(true);
     }
     else if (wave == WaveType.Noise) {
       noiseButton.SetGlow(true);
@@ -52,8 +52,8 @@ public class Oscillator : SignalNode {
 
   public override void OnButtonClick(Button button) {
     sineButton.SetGlow(false);
-    squareButton.SetGlow(false);
-    sawtoothButton.SetGlow(false);
+    pulseButton.SetGlow(false);
+    slopedButton.SetGlow(false);
     noiseButton.SetGlow(false);
 
     button.SetGlow(true);
@@ -61,11 +61,11 @@ public class Oscillator : SignalNode {
     if (button == sineButton) {
       wave = WaveType.Sine;
     }
-    else if (button == squareButton) {
-      wave = WaveType.Square;
+    else if (button == pulseButton) {
+      wave = WaveType.Pulse;
     }
-    else if (button == sawtoothButton) {
-      wave = WaveType.Sawtooth;
+    else if (button == slopedButton) {
+      wave = WaveType.Sloped;
     }
     else if (button == noiseButton) {
       wave = WaveType.Noise;
@@ -75,41 +75,49 @@ public class Oscillator : SignalNode {
   public override double[] GetValues(double sample, int count, Stack<SignalNode> nodes) {
     double[] values = new double[count];
 
-    double[] frequencyAdjustValues = frequencyAdjustInput.IsConnected() ?
-      frequencyAdjustInput.GetValues(sample, count, nodes) :
+    double[] frequencyAdjustValues = frequencyInput.IsConnected() ?
+      frequencyInput.GetValues(sample, count, nodes) :
       Enumerable.Repeat(0d, count).ToArray();
-    double[] volumeAdjustValues = volumeAdjustInput.IsConnected() ?
-      volumeAdjustInput.GetValues(sample, count, nodes) :
+    double[] volumeAdjustValues = volumeInput.IsConnected() ?
+      volumeInput.GetValues(sample, count, nodes) :
+      Enumerable.Repeat(0d, count).ToArray();
+    double[] waveformAdjustValues = waveformInput.IsConnected() ?
+      waveformInput.GetValues(sample, count, nodes) :
       Enumerable.Repeat(0d, count).ToArray();
 
     float frequency = frequencyKnob.value;
+    float volume = volumeKnob.value;
+    float waveform = waveformKnob.value;
 
     for (int i = 0; i < count; i++) {
+      double currentFrequency = frequency + frequencyAdjustValues[i];
+      float currentWaveform = (float)(waveform + waveformAdjustValues[i]);
+      float currentVolume = (float)(volume * (1 + volumeAdjustValues[i]));
+
       double thisSample = sample + i;
       double sampleIncrement = thisSample - lastSample;
       lastSample = thisSample;
-
-      double currentFrequency = frequency + frequencyAdjustValues[i];
-      double increment = sampleIncrement * currentFrequency / sampleFrequency;
-
-      phase = (phase + increment) % 1;
+      double phaseIncrement = sampleIncrement * currentFrequency / sampleFrequency;
+      phase = (phase + phaseIncrement) % 1;
 
       double value = 0;
 
       if (wave == WaveType.Sine) {
         value = Mathf.Sin((float)(phase * TAU));
       }
-      else if (wave == WaveType.Square) {
-        value = Mathf.Sign(Mathf.Sin((float)(phase * TAU)));
+      else if (wave == WaveType.Pulse) {
+        value = phase >= currentWaveform ? -1 : 1;
       }
-      else if (wave == WaveType.Sawtooth) {
-        value = phase * 2 - 1;
+      else if (wave == WaveType.Sloped) {
+        value = phase < currentWaveform ?
+          Mathf.Lerp(-1, 1, Mathf.InverseLerp(0, currentWaveform, (float)phase)) :
+          Mathf.Lerp(1, -1, Mathf.InverseLerp(currentWaveform, 1, (float)phase));
       }
       else if (wave == WaveType.Noise) {
         value = rand.NextDouble() * 2 - 1;
       }
 
-      values[i] = value * (1 + volumeAdjustValues[i]) * volume;
+      values[i] = value * currentVolume;
     }
 
     return values;
