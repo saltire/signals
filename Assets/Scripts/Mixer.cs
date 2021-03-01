34118b5433b2355f30a64ccaa59947b87169a74e
saltire sable <saltiresable@gmail.com>
2021-02-28 21:10:51 -0500

    Add multiply (ring mod) mode to mixer

--- Assets/Scripts/Mixer.cs
+++ Assets/Scripts/Mixer.cs
@@ -3,20 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+public enum MixType {
+  Add,
+  Multiply,
+}
+
 public class Mixer : SignalModule {
   SignalInput[] inputs;
   RangeControl fader;
+  public Button addButton;
+  public Button multiplyButton;
+
+  public MixType mixType = MixType.Add;
 
   void Awake() {
     inputs = GetComponentsInChildren<SignalInput>();
     fader = GetComponentInChildren<RangeControl>();
+
+    if (mixType == MixType.Add) {
+      addButton.SetGlow(true);
+    }
+    else if (mixType == MixType.Multiply) {
+      multiplyButton.SetGlow(true);
+    }
+  }
+
+  public override void OnButtonClick(Button button) {
+    button.SetGlow(true);
+
+    if (button == addButton) {
+      mixType = MixType.Add;
+      multiplyButton.SetGlow(false);
+    }
+    else if (button == multiplyButton) {
+      mixType = MixType.Multiply;
+      addButton.SetGlow(false);
+    }
   }
 
   float[] GetMultipliers() {
     // Fader range should go from -1 to 1.
-    return new float[] {
-      Mathf.Min(-fader.value + 1, 1),
-      Mathf.Min(fader.value + 1, 1),
+    return new[] {
+      Mathf.InverseLerp(1, 0, fader.value),
+      Mathf.InverseLerp(-1, 0, fader.value),
     };
   }
 
@@ -24,6 +53,10 @@ public class Mixer : SignalModule {
     double[][] values = inputs.Select(i => i.GetValues(sample, count, modules)).ToArray();
     float[] mult = GetMultipliers();
 
-    return values[0].Zip(values[1], (v0, v1) => v0 * mult[0] + v1 * mult[1]).ToArray();
+    return values[0].Zip(values[1], (v0, v1) => {
+      double mv0 = v0 * mult[0];
+      double mv1 = v1 * mult[1];
+      return mixType == MixType.Multiply ? mv0 * mv1 : mv0 + mv1;
+    }).ToArray();
   }
 }
